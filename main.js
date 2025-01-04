1db592a0da62e04a4fad430f3b1d2132e3dcfe7e
vipin018 <vipinpathak018@gmail.com>
2025-01-04 13:22:33 +0530

    setup

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import vertexShader from './shaders/vertexShader.glsl';
+import fragmentShader from './shaders/fragmentShader.glsl';
 
 // Scene setup
 const scene = new THREE.Scene();
@@ -25,34 +27,25 @@ function onWindowResize() {
 }
 
 // Icosahedron geometry
-const geometry = new THREE.IcosahedronGeometry(2.5, 20, 20);
-
-// Shader material
-const vertexShader = `
-    void main() {
-        gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-    }
-`;
-
-const fragmentShader = `
-    void main() {
-        gl_FragColor = vec4(1.0, 0.5, 0.0, 1.0); // Orange color
-    }
-`;
+const geometry = new THREE.IcosahedronGeometry(1, 20);
 
 const material = new THREE.ShaderMaterial({
-  vertexShader,
-  fragmentShader,
-  wireframe: true, // Uncomment to show wireframe representation of the icosahedron
+  
+vertexShader,
+fragmentShader,
+  // wireframe: true, // Uncomment to show wireframe representation of the icosahedron
+  uniforms: {
+    uTime: {  value: 0 },
+  },
 });
 
 // Create mesh
 const icosahedron = new THREE.Mesh(geometry, material);
-icosahedron.position.y =-2.9;
+icosahedron.position.y =-1.3;
 scene.add(icosahedron);
 
 // Camera position
-camera.position.z =4.5;
+camera.position.z =2;
 
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true; // an animation loop is required when either damping or auto-rotation are enabled
@@ -60,12 +53,14 @@ controls.dampingFactor = 0.25;
 controls.screenSpacePanning = false;
 controls.maxPolarAngle = Math.PI / 2;
 
+const clock = new THREE.Clock();
 
 // Animation loop
 function animate() {
   requestAnimationFrame(animate);
   controls.update(); // only if controls.enableDamping = true
   // icosahedron.rotation.x += 0.01;
+  material.uniforms.uTime.value= clock.getElapsedTime();
   renderer.render(scene, camera);
 }
 
